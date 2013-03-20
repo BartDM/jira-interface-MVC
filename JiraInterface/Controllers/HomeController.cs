90950a92f09824f5ee50a604d2dfb3582f6c4031
Bart De Meyer <bart@bartdemeyer.be>
2013-03-20 13:40:46 +0100

    Added project

---
+++ JiraInterface/Controllers/HomeController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Atlassian.Jira;
+using JiraInterface.Models;
+
+namespace JiraInterface.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Jira jira;
+        private IEnumerable<IssuePriority> priorities;
+        private IEnumerable<IssueStatus> statusses;
+
+        public HomeController()
+        {
+            var credentials = Helpers.Data.GetJiraCredentials();
+            jira = new Jira(credentials.Item1, credentials.Item2, credentials.Item3);
+        }
+
+        //
+        // GET: /Home/
+
+        private void CheckStatusses()
+        {
+            if (statusses == null)
+                statusses = jira.GetIssueStatuses();
+        }
+
+        private void CheckPriorities()
+        {
+            if (priorities == null)
+                priorities = jira.GetIssuePriorities();
+        }
+
+        public ActionResult Index()
+        {
+            CheckPriorities();
+            CheckStatusses();
+
+            var issues = jira.GetIssuesFromFilter("Z_Group_Public");
+
+            ViewBag.Priorities = priorities;
+            ViewBag.Statusses = statusses;
+
+
+            return View(issues);
+        }
+
+        public ActionResult Create()
+        {
+            //Build the priorities manual
+            var extendedPriorities = new List<ExtendedPriority>();
+            extendedPriorities.Add(new ExtendedPriority()
+                {
+                    Id = 1,
+                    Name = "Blocker",
+                    Description = "De ZGMS is onbruikbaar"
+                });
+            extendedPriorities.Add(new ExtendedPriority()
+                {
+                    Id = 2,
+                    Name = "Critical",
+                    Description = "De ZGMS is bruikbaar op zich maar sleutel mogelijkheden lukken niet"
+                });
+            extendedPriorities.Add(new ExtendedPriority()
+                {
+                    Id = 3,
+                    Name = "Major",
+                    Description = "De ZGMS is volledig bruikbaar maar deze fout moet dringend worden opgelost"
+                });
+            extendedPriorities.Add(new ExtendedPriority()
+                {
+                    Id = 4,
+                    Name = "Minor",
+                    Description = "De ZGMS is volledig bruikbaar en deze fout moet in een volgende versie worden opgelost"
+                });
+            extendedPriorities.Add(new ExtendedPriority()
+                {
+                    Id = 5,
+                    Name = "Trivial",
+                    Description = "De ZGMS is volledig bruikbaar en deze fout moet bekeken en besproken worden"
+                });
+            extendedPriorities.Add(new ExtendedPriority()
+                {
+                    Id = 7,
+                    Name = "New scope",
+                    Description =
+                        "De ZGMS is volledig bruikbaar zoals beschreven maar er moet functionaliteit aangepast worden"
+                });
+
+            ViewBag.Priorities = extendedPriorities;
+
+            var version = jira.GetProjectVersions("ZGRP").FirstOrDefault(v => !v.IsArchived && !v.IsReleased);
+            ViewBag.Version = version;
+
+            var chapters = Helpers.Data.GetChapters();
+            ViewBag.Chapters = chapters;
+
+            return View();
+        }
+
+        //
+        // POST: /Home/Create
+
+        [HttpPost]
+        public ActionResult Create(CreateIssue issue)
+        {
+            CheckPriorities();
+            CheckStatusses();
+            try
+            {
+                var jiraIssue = jira.CreateIssue("ZGRP");
+                jiraIssue.Type = "Bug";
+
+                var priority =
+                    priorities.FirstOrDefault(p => p.Id == issue.Priority.ToString(CultureInfo.InvariantCulture));
+                jiraIssue.Priority = priority != null ? priority.Name : "Minor";
+                jiraIssue.Summary = issue.Title;
+                var sb = new StringBuilder();
+                sb.AppendFormat("Gemeld door {0} - {1}- {2}. {3}", issue.Reporter, issue.Email,
+                                issue.Tel, Environment.NewLine);
+                sb.AppendFormat("Document referentie: {0} - {1} {2}", issue.DocumentChapter, issue.DocumentItem,
+                                Environment.NewLine);
+                sb.Append(issue.Description);
+                jiraIssue.Description = sb.ToString();
+                var version = jira.GetProjectVersions("ZGRP").FirstOrDefault(v => !v.IsArchived && !v.IsReleased);
+                if (version != null)
+                    jiraIssue.AffectsVersions.Add(version);
+                jiraIssue.SaveChanges();
+
+                Mail(issue.Title);
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("Error");
+            }
+        }
+
+        private void Mail(string title)
+        {
+            var emailAddresses = Helpers.Data.GetEmailAddresses();
+
+
+            var mm = new MailMessage(emailAddresses.Item1, emailAddresses.Item2);
+            mm.Body = string.Format("ZGROUP: Er is een nieuwe onvolkomenheid gemeld: {0}", title);
+            mm.BodyEncoding = new UTF8Encoding();
+            mm.IsBodyHtml = false;
+            mm.Subject = mm.Body;
+
+            var client = new SmtpClient();
+            client.Send(mm);
+        }
+    }
+}
---
+++ JiraInterface/Helpers/Data.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace JiraInterface.Helpers
+{
+    public class Data
+    {
+        public static List<string> GetChapters()
+        {
+            var mapPath = AppDomain.CurrentDomain.GetData("DataDirectory");
+            XDocument doc = XDocument.Load(Path.Combine(mapPath.ToString(), "Chapters.xml"));
+            var items = doc.Root.Elements("Item").Select(e => e.Attribute("nr").Value + " " + e.Value).ToList();
+            return items;
+        }
+
+        public static Tuple<string,string,string> GetJiraCredentials()
+        {
+            var mapPath = AppDomain.CurrentDomain.GetData("DataDirectory");
+            XDocument doc = XDocument.Load(Path.Combine(mapPath.ToString(), "JiraCredentials.xml"));
+            var url = doc.Root.Element("Url") != null ? doc.Root.Element("Url").Value: string.Empty;
+            var user = doc.Root.Element("User") != null ? doc.Root.Element("User").Value: string.Empty;
+            var password = doc.Root.Element("Password") != null ? doc.Root.Element("Password").Value : string.Empty;
+
+            return new Tuple<string,string, string>(url,user,password);
+        }
+
+        public static Tuple<string,string> GetEmailAddresses()
+        {
+            var mapPath = AppDomain.CurrentDomain.GetData("DataDirectory");
+            XDocument doc = XDocument.Load(Path.Combine(mapPath.ToString(), "JiraCredentials.xml"));
+            var mailFrom = doc.Root.Element("MailFrom") != null ? doc.Root.Element("MailFrom").Value : string.Empty;
+            var mailTo = doc.Root.Element("MailTo") != null ? doc.Root.Element("MailTo").Value : string.Empty;
+
+            return new Tuple<string, string>(mailFrom,mailTo);
+        }
+    }
+}
